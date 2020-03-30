e0e141dfa4243311afd7a96b2e8a32c8784295d2
Geoff Ower <gdower@illinois.edu>
2020-03-30 17:18:25 -0500

    Added nom code and accordingTo

--- scripts/convert.py
+++ scripts/convert.py
@@ -3,8 +3,10 @@ import os
 # Download data
 os.system('cd /home/col/raw; wget -c -N https://www.itis.gov/downloads/itisMySQLBulk.zip')
 
+os.system('sleep 4')
+
 # Unzip data
-os.system('cd /home/col/raw; unzip itisMySQLBulk.zip')
+os.system('cd /home/col/raw; unzip -o itisMySQLBulk.zip')
 
 # Import data into MySQL
 os.system('cd /home/col/raw/itisMySQL022620; mysql -hdatabase -uroot -pbiodiversity < CreateDB.sql')
@@ -12,3 +14,11 @@ os.system('cd /home/col/raw/itisMySQL022620; mysql -hdatabase -uroot -pbiodivers
 # Convert the data to CoLDP
 os.system('cd /home/col/scripts; cat convert.sql | mysql -hdatabase -uroot -pbiodiversity')
 
+# Dump tsv files
+#os.system('cd /home/col/coldp; mysqldump -hdatabase -uroot -pbiodiversity --tab=/home/col/coldp coldp')
+os.system('cd /home/col/coldp; mysql -B -uroot -pbiodiversity -hdatabase coldp -e "SELECT * FROM Name" > Name.tsv')
+os.system('cd /home/col/coldp; mysql -B -uroot -pbiodiversity -hdatabase coldp -e "SELECT * FROM Taxon" > Taxon.tsv')
+os.system('cd /home/col/coldp; mysql -B -uroot -pbiodiversity -hdatabase coldp -e "SELECT * FROM Synonym" > Synonym.tsv')
+os.system('cd /home/col/coldp; mysql -B -uroot -pbiodiversity -hdatabase coldp -e "SELECT * FROM VernacularName" > VernacularName.tsv')
+os.system('cd /home/col/coldp; mysql -B -uroot -pbiodiversity -hdatabase coldp -e "SELECT * FROM Distribution" > Distribution.tsv')
+os.system('cd /home/col/coldp; mysql -B -uroot -pbiodiversity -hdatabase coldp -e "SELECT * FROM Reference" > Reference.tsv')
--- scripts/convert.sql
+++ scripts/convert.sql
@@ -1,5 +1,6 @@
 USE ITIS;
 
+DROP DATABASE IF EXISTS coldp;
 CREATE DATABASE coldp DEFAULT CHARSET utf8mb4 COLLATE utf8mb4_general_ci;
 
 # TODO: Provisional
@@ -11,18 +12,21 @@ CREATE TABLE coldp.Taxon (
            h.Parent_TSN AS parentID,
            h.TSN AS nameID,
            FALSE AS provisional,
-           NULL AS accordingTo,
+           (SELECT DISTINCT GROUP_CONCAT(expert SEPARATOR ', ') FROM reference_links rl INNER JOIN experts ON rl.documentation_id=experts.expert_id AND rl.doc_id_prefix='EXP' WHERE rl.tsn=h.TSN) AS accordingTo,
            NULL AS accordingToID,
            tu.update_date AS accordingToDate,
            (SELECT DISTINCT GROUP_CONCAT(documentation_id SEPARATOR ', ') FROM reference_links rl WHERE rl.tsn=h.TSN AND doc_id_prefix='PUB') AS referenceID
     FROM hierarchy h LEFT JOIN taxonomic_units tu ON h.TSN = tu.tsn
 );
 
-SELECT DISTINCT unaccept_reason FROM taxonomic_units;
 
+#SELECT tsn, expert FROM experts INNER JOIN reference_links rl ON experts.expert_id = rl.documentation_id AND experts.expert_id_prefix = rl.doc_id_prefix GROUP BY tsn HAVING count(*) > 1;
+
+#SELECT DISTINCT unaccept_reason FROM taxonomic_units;
+#SELECT DISTINCT n_usage, kingdom_id FROM taxonomic_units;
+#SELECT DISTINCT name_usage FROM taxonomic_units;
 
 # TODO: Hybrid formulas
-# TODO: Nom code
 # TODO: Nom status
 # TODO: Nom ref
 # TODO: Nom original
@@ -37,6 +41,7 @@ CREATE TABLE coldp.Name (
            NULL AS infragenericEpithet,
            NULL AS specificEpithet,
            NULL AS infraspeciesEpithet,
+           CASE WHEN tu.kingdom_id=1 THEN 'bacterial' WHEN tu.kingdom_id=2 THEN 'zoological' WHEN tu.kingdom_id=3 THEN 'botanical' WHEN tu.kingdom_id=4 THEN 'botanical' WHEN tu.kingdom_id=5 THEN 'zoological' WHEN tu.kingdom_id=6 THEN 'botanical' WHEN tu.kingdom_id=7 THEN 'bacterial' END AS code,
            CONCAT('https://www.itis.gov/servlet/SingleRpt/SingleRpt?search_topic=TSN&search_value=', TSN) AS link
     FROM taxonomic_units tu
         LEFT JOIN taxon_authors_lkp tal ON tu.taxon_author_id = tal.taxon_author_id
@@ -51,6 +56,7 @@ CREATE TABLE coldp.Name (
            NULL       AS infragenericEpithet,
            unit_name2 AS specificEpithet,
            unit_name3 AS infraspeciesEpithet,
+           CASE WHEN tu.kingdom_id=1 THEN 'bacterial' WHEN tu.kingdom_id=2 THEN 'zoological' WHEN tu.kingdom_id=3 THEN 'botanical' WHEN tu.kingdom_id=4 THEN 'botanical' WHEN tu.kingdom_id=5 THEN 'zoological' WHEN tu.kingdom_id=6 THEN 'botanical' WHEN tu.kingdom_id=7 THEN 'bacterial' END AS code,
            CONCAT('https://www.itis.gov/servlet/SingleRpt/SingleRpt?search_topic=TSN&search_value=', TSN) AS link
     FROM taxonomic_units tu
         LEFT JOIN taxon_authors_lkp tal ON tu.taxon_author_id = tal.taxon_author_id
@@ -65,6 +71,7 @@ CREATE TABLE coldp.Name (
            unit_name2 AS infragenericEpithet,
            unit_name3 AS specificEpithet,
            unit_name4 AS infraspeciesEpithet,
+           CASE WHEN tu.kingdom_id=1 THEN 'bacterial' WHEN tu.kingdom_id=2 THEN 'zoological' WHEN tu.kingdom_id=3 THEN 'botanical' WHEN tu.kingdom_id=4 THEN 'botanical' WHEN tu.kingdom_id=5 THEN 'zoological' WHEN tu.kingdom_id=6 THEN 'botanical' WHEN tu.kingdom_id=7 THEN 'bacterial' END AS code,
            CONCAT('https://www.itis.gov/servlet/SingleRpt/SingleRpt?search_topic=TSN&search_value=', TSN) AS link
     FROM taxonomic_units tu
         LEFT JOIN taxon_authors_lkp tal ON tu.taxon_author_id = tal.taxon_author_id
