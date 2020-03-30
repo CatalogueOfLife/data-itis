1b7b9980efd605d145ed4b3e358300f6691e0c52
Geoff Ower <gdower@illinois.edu>
2020-03-30 10:35:35 -0500

    Run conversion automatically

--- scripts/convert.py
+++ scripts/convert.py
@@ -8,3 +8,7 @@ os.system('cd /home/col/raw; unzip itisMySQLBulk.zip')
 
 # Import data into MySQL
 os.system('cd /home/col/raw/itisMySQL022620; mysql -hdatabase -uroot -pbiodiversity < CreateDB.sql')
+
+# Convert the data to CoLDP
+os.system('cd /home/col/scripts; cat convert.sql | mysql -hdatabase -uroot -pbiodiversity')
+
--- scripts/convert.sql
+++ scripts/convert.sql
@@ -1,3 +1,4 @@
+USE ITIS;
 
 CREATE DATABASE coldp DEFAULT CHARSET utf8mb4 COLLATE utf8mb4_general_ci;
 
