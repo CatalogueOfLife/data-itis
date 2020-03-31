d1ad24831a318a3c273fa0e5ba6125a34f8b503a
Geoff Ower <gdower@illinois.edu>
2020-03-31 11:14:57 -0500

    Add status messages

--- scripts/convert.py
+++ scripts/convert.py
@@ -1,21 +1,25 @@
 import os
 
 # Download data
+os.system('echo "Checking for updated ITIS download..."')
 os.system('cd /home/col/raw; wget -c -N https://www.itis.gov/downloads/itisMySQLBulk.zip')
-
 os.system('sleep 4')
 
 # Unzip data
+os.system('echo "Uncompressing ITIS data..."')
 os.system('cd /home/col/raw; unzip -o itisMySQLBulk.zip')
 
 # Import data into MySQL
+os.system('echo "Importing ITIS data into MySQL database..."')
 os.system('cd /home/col/raw/itisMySQL022620; mysql -hdatabase -uroot -pbiodiversity < CreateDB.sql')
 
 # Convert the data to CoLDP
+os.system('echo "Converting ITIS data to CoLDP format..."')
 os.system('cd /home/col/scripts; cat convert.sql | mysql -hdatabase -uroot -pbiodiversity')
 
 # Dump tsv files
 #os.system('cd /home/col/coldp; mysqldump -hdatabase -uroot -pbiodiversity --tab=/home/col/coldp coldp')
+os.system('echo "Exporting data to tab delimited files in CoLDP format..."')
 os.system('cd /home/col/coldp; mysql -B -uroot -pbiodiversity -hdatabase coldp -e "SELECT * FROM Name" > Name.tsv')
 os.system('cd /home/col/coldp; mysql -B -uroot -pbiodiversity -hdatabase coldp -e "SELECT * FROM Taxon" > Taxon.tsv')
 os.system('cd /home/col/coldp; mysql -B -uroot -pbiodiversity -hdatabase coldp -e "SELECT * FROM Synonym" > Synonym.tsv')
