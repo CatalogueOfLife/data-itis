48157e48cd78611a52427d96e9e4163225b4248b
Geoff Ower <gdower@illinois.edu>
2020-04-01 13:13:38 -0500

    Get MySQL password from user

--- docker/conversion.Dockerfile
+++ docker/conversion.Dockerfile
@@ -1,11 +1,11 @@
-FROM python:3.7
+FROM python:3.8
   
 ENV HOME /home/col
 ENV PATH /home/col/go/bin:$PATH
 WORKDIR /home/col
 
 # Install software dependencies
-RUN apt-get update -y && apt-get install -y default-mysql-client python3-pip python3-dev mdbtools curl git wget unzip
+RUN apt-get update -y && apt-get install -y default-libmysqlclient-dev default-mysql-client default-mysql-client-core python3-pip python3-dev mdbtools curl git wget unzip
 
 # Install python dependencies
 RUN pip install --upgrade pip && \
--- scripts/convert.py
+++ scripts/convert.py
@@ -1,9 +1,13 @@
 import os
 
+# Get database password
+DATABASE_PASSWORD = os.environ.get('DATABASE_PASSWORD')
+print(DATABASE_PASSWORD)
+
 # Download data
 os.system('echo "Checking for updated ITIS download..."')
 os.system('cd /home/col/raw; wget -c -N https://www.itis.gov/downloads/itisMySQLBulk.zip')
-os.system('sleep 4')
+os.system('sleep 10')
 
 # Unzip data
 os.system('echo "Uncompressing ITIS data..."')
@@ -11,18 +15,20 @@ os.system('cd /home/col/raw; unzip -o itisMySQLBulk.zip')
 
 # Import data into MySQL
 os.system('echo "Importing ITIS data into MySQL database..."')
-os.system('cd /home/col/raw/itisMySQL022620; mysql -hdatabase -uroot -pbiodiversity < CreateDB.sql')
+os.system('cd /home/col/raw/itisMySQL022620; mysql -hdatabase -uroot -p' + DATABASE_PASSWORD + ' < CreateDB.sql')
 
 # Convert the data to CoLDP
 os.system('echo "Converting ITIS data to CoLDP format..."')
-os.system('cd /home/col/scripts; cat convert.sql | mysql -hdatabase -uroot -pbiodiversity')
+os.system('cd /home/col/scripts; cat convert.sql | mysql -hdatabase -uroot -p' + DATABASE_PASSWORD)
 
 # Dump tsv files
-#os.system('cd /home/col/coldp; mysqldump -hdatabase -uroot -pbiodiversity --tab=/home/col/coldp coldp')
+#os.system('cd /home/col/coldp; mysqldump -hdatabase -uroot -p' + DATABASE_PASSWORD + ' --tab=/home/col/coldp coldp')
 os.system('echo "Exporting data to tab delimited files in CoLDP format..."')
-os.system('cd /home/col/coldp; mysql -B -uroot -pbiodiversity -hdatabase coldp -e "SELECT * FROM Name" > Name.tsv')
-os.system('cd /home/col/coldp; mysql -B -uroot -pbiodiversity -hdatabase coldp -e "SELECT * FROM Taxon" > Taxon.tsv')
-os.system('cd /home/col/coldp; mysql -B -uroot -pbiodiversity -hdatabase coldp -e "SELECT * FROM Synonym" > Synonym.tsv')
-os.system('cd /home/col/coldp; mysql -B -uroot -pbiodiversity -hdatabase coldp -e "SELECT * FROM VernacularName" > VernacularName.tsv')
-os.system('cd /home/col/coldp; mysql -B -uroot -pbiodiversity -hdatabase coldp -e "SELECT * FROM Distribution" > Distribution.tsv')
-os.system('cd /home/col/coldp; mysql -B -uroot -pbiodiversity -hdatabase coldp -e "SELECT * FROM Reference" > Reference.tsv')
+os.system('cd /home/col/coldp; mysql -B -uroot -p' + DATABASE_PASSWORD + ' -hdatabase coldp -e "SELECT * FROM Name" > Name.tsv')
+os.system('cd /home/col/coldp; mysql -B -uroot -p' + DATABASE_PASSWORD + ' -hdatabase coldp -e "SELECT * FROM Taxon" > Taxon.tsv')
+os.system('cd /home/col/coldp; mysql -B -uroot -p' + DATABASE_PASSWORD + ' -hdatabase coldp -e "SELECT * FROM Synonym" > Synonym.tsv')
+os.system('cd /home/col/coldp; mysql -B -uroot -p' + DATABASE_PASSWORD + ' -hdatabase coldp -e "SELECT * FROM VernacularName" > VernacularName.tsv')
+os.system('cd /home/col/coldp; mysql -B -uroot -p' + DATABASE_PASSWORD + ' -hdatabase coldp -e "SELECT * FROM Distribution" > Distribution.tsv')
+os.system('cd /home/col/coldp; mysql -B -uroot -p' + DATABASE_PASSWORD + ' -hdatabase coldp -e "SELECT * FROM Reference" > Reference.tsv')
+
+os.system('sleep 10000')
